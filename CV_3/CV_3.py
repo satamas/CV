51006dddae54b58cb4d0c82e430565ed78902a01
satamas <se.atamas@gmail.com>
2014-12-17 11:53:11 +0300

    FFT mask fixed

--- CV_3/CV_3.py
+++ CV_3/CV_3.py
@@ -12,12 +12,13 @@ fft = np.fft.fft2(img)
 
 center = (fft.shape[0] / 2, fft.shape[1] / 2)
 mask = np.zeros(fft.shape)
-mask[CUTOFF_FREQUENCY:fft.shape[0] - CUTOFF_FREQUENCY,
-     CUTOFF_FREQUENCY:fft.shape[1] - CUTOFF_FREQUENCY] = 1
+mask[CUTOFF_FREQUENCY:fft.shape[0] - CUTOFF_FREQUENCY, 0:fft.shape[1]] = 1
+mask[0:fft.shape[0], CUTOFF_FREQUENCY:fft.shape[1] - CUTOFF_FREQUENCY] = 1
+
 filtered_fft = fft * mask
 
 processedImage = np.fft.ifft2(filtered_fft)
-cv2.imwrite('filtered.bmp', np.real(processedImage))
+cv2.imwrite('filtered.bmp', np.abs(processedImage))
 
 cv2.imwrite('laplassian.bmp', cv2.Laplacian(img, cv2.CV_32F))
 
